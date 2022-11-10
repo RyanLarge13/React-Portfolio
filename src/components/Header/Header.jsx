da5b5d94fd688512bfdc0a3eb5b9d7d14407003b
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2022-11-10 10:02:18 -0800

    Updating the Header component which contains the cloud download button for fetching my resume from the server via onClick, Axios & FileDownload/js-file-download npm package. The function runs a get request and downloads my resume as a pdf file.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,25 @@
 import { AiOutlineCloudDownload } from "react-icons/ai";
+import Axios from "axios";
+import FileDownload from "js-file-download";
 import "./header.scss";
+
+const dlPdf = async (e) => {
+  e.preventDefault();
+  await Axios({
+    url: "https://react-folio-server.onrender.com/pdf",
+    method: "GET",
+    responseType: "blob",
+  }).then((res) => {
+    FileDownload(res.data, "RyanLargesResume.pdf");
+  });
+};
 
 const Header = () => {
   return (
     <>
       <header>
         <p className="logo">Ryan } /></p>
-        <AiOutlineCloudDownload className="download" />
+        <AiOutlineCloudDownload className="download" onClick={(e) => dlPdf(e)} />
       </header>
     </>
   );
